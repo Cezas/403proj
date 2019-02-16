6bb5f35b19de702ff72b4d16de865703d7491cb0
Cezas <robertyan97@hotmail.com>
2019-02-16 16:29:03 -0500

    updated detection timer to be manually set

--- pi-face-recognition/test.py
+++ pi-face-recognition/test.py
@@ -1,26 +1,15 @@
+#creds to pyimagesearch for boilerplate code
 # USAGE
 # python test.py --cascade haarcascade_frontalface_default.xml --encodings encodings.pickle
 
-#TODOTODOTODOTODOTODO CURRENT BUGS AND SHIT TODO TODO TODO
+#TODOTODOTODOTODOTODOTODOTODOTODO
 
-#TODO: measure time it takes to detect faces
+#refactor and clean up
+#incorporate a hard refresh of the tracker as it can get stuck on differnt things (on Mosse)
+	#accomplish this by checking "unknown" vs "dataset"
+#incorporate multiple haar cascade xmls
 
-#TODO: begin target selection algo
-
-#TODO: switch over to generic object detection
-
-#even if the tracker initializes properly, it might still fail
-
-#RESOLVED - once tracker loses its first catch, it will no longer work.
-#RESOLVED - so, how do i reset the tracker?
-
-#TODOTODOTODOTODOTODOTODOTODOTODOTODOTODOTODO TODO TODO TODO
-
-
-
-
-
-
+#TODOTODOTODOTODOTODOTODOTODOTODO
 
 # import the necessary packages
 from imutils.video import VideoStream
@@ -41,17 +30,15 @@ ap.add_argument("-e", "--encodings", required=True,
 	help="path to serialized db of facial encodings")
 args = vars(ap.parse_args())
 
-#**********ADDING IN TRACKING
+#**********INITIALIZING TRACKER AND VARIOUS VARS
 #tracker = cv2.TrackerKCF_create()
 tracker = cv2.TrackerMOSSE_create()
-initBB = None
-success = False
-initialized = False
-
-#*********END CHANGES
-
-
-
+initBB = None #initial bounding box that will encapsulate the object
+success = False #denotes on whether or not a detection occurred
+initialized = False #denotes whether or not the tracker was initialized
+detectrate = 270  #how many frames pass until object detection force checks again
+starttime = None #initially none, will be manually determined thru user input
+#*********END INIT TRACKER
 
 # load the known faces and embeddings along with OpenCV's Haar
 # cascade for face detection
@@ -62,15 +49,15 @@ detector = cv2.CascadeClassifier(args["cascade"])
 # initialize the video stream and allow the camera sensor to warm up
 print("[INFO] starting video stream...")
 vs = VideoStream(usePiCamera=1).start()
-# vs = VideoStream(usePiCamera=True).start()
 time.sleep(2.0)
 
 # start the FPS counter
 fps = FPS().start()
 framecounter = 0
 
-starttime = time.time()
 
+
+#********************MAIN ALGO********************
 # loop over frames from the video file stream
 while True:
 	framecounter = framecounter+1
@@ -78,8 +65,8 @@ while True:
 	# to 500px (to speedup processing)
 	frame = vs.read()
 	frame = imutils.resize(frame, width=500)
-	(H, W) = frame.shape[:2]	
-	 # check to see if we are currently tracking an objec
+	(H, W) = frame.shape[:2]  	
+	 # check to see if we are currently tracking an object
 	if initBB is not None:
 		#print('INITBB is',initBB)
 		# grab the new bounding box coordinates of the object
@@ -87,17 +74,15 @@ while True:
 		#print("TRACKER gives", box)
                 # check to see if the tracking was a success
 		if success:
-			###this seems to fail regardless of what initBB is
-			#(top, right, bottom, left) = [int(v) for v in box]
-			#cv2.rectangle(frame, (left, top), (right, bottom),(0, 255, 0), 2)
-
 			(x, y, w, h) = [int(v) for v in box]
 			cv2.rectangle(frame, (x, y), (x + w, y + h),
                               (0, 255, 0), 2)
+		#***********if there is no current thing being tracked, restart the tracker entirely
 		else:
 			tracker = cv2.TrackerMOSSE_create()
 			initBB = None
 			initialized = False
+		#**************
 		# update the FPS counter
 		fps.update()
 		fps.stop()
@@ -107,7 +92,7 @@ while True:
 		info = [
                         ("Tracker","variable"),
                         ("Success", "Yes" if success else "No"),
-                        ("FPS", "{:.2f}".format(fps.fps())),
+                        ("Avg FPS", "{:.2f}".format(fps.fps())),
 		]
 
                	# loop over the info tuples and draw them on our frame
@@ -116,8 +101,8 @@ while True:
 			cv2.putText(frame, text, (10, H - ((i * 20) + 20)),cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 0, 255), 2)
 	
 
-	#*******attempt object recognition every 20 frames and  when target is lost      
-	if framecounter%20==0 and not success:
+	#*******attempt object recognition every N frames and when target is undetected      
+	if framecounter%detectrate==1 or not success: #i do %==1 as %==0 would have this go off a lot initially
 		print('#######ATTEMPTING DETECTION##########')	
 		# convert the input frame from (1) BGR to grayscale (for face
 		# detection) and (2) from BGR to RGB (for face recognition)
@@ -128,50 +113,52 @@ while True:
 		rects = detector.detectMultiScale(gray, scaleFactor=1.1, 
 			minNeighbors=5, minSize=(30, 30),
 			flags=cv2.CASCADE_SCALE_IMAGE)
-	
+
 		# OpenCV returns bounding box coordinates in (x, y, w, h) order
 		# but we need them in (top, right, bottom, left) order, so we
 		# need to do a bit of reordering
 		boxes = [(y, x + w, y + h, x) for (x, y, w, h) in rects]
 		
-
-
 		# compute the facial embeddings for each face bounding box
 		encodings = face_recognition.face_encodings(rgb, boxes)
 		names = []
-		
+
 		# loop over the facial embeddings	
 		for encoding in encodings:
 			# attempt to match each face in the input image to our known
 			# encodings
 			matches = face_recognition.compare_faces(data["encodings"],encoding)
 			name = "Unknown"
-		
-				# check to see if we have found a match
-			if True in matches:
-				#match detected, mark time
+
+			#********match detected, mark time
+			if starttime is not None:
 				print('Face detected in',round(time.time()-starttime,2),'seconds')
-				starttime = time.time()
-				# find the indexes of all matched faces then initialize a
+				starttime = None
+			#**********
+
+			
+			# check to see if we have found a match
+			if True in matches:
+				
+				#iterare through the				
 				# dictionary to count the total number of times each face
 				# was matched
 				matchedIdxs = [i for (i, b) in enumerate(matches) if b]
 				counts = {}
-		
+
 				# loop over the matched indexes and maintain a count for
 				# each recognized face face
 				for i in matchedIdxs:
 					name = data["names"][i]
 					counts[name] = counts.get(name, 0) + 1
-		
+
 				# determine the recognized face with the largest number
 				# of votes (note: in the event of an unlikely tie Python
 				# will select first entry in the dictionary)
 				name = max(counts, key=counts.get)
-			
+
 			# update the list of names
 			names.append(name)
-		
 			# loop over the recognized faces
 			for ((top, right, bottom, left), name) in zip(boxes, names):
 				# draw the predicted face name on the image
@@ -181,31 +168,29 @@ while True:
 				cv2.putText(frame, name, (left, y), cv2.FONT_HERSHEY_SIMPLEX,
 					0.75, (0, 255, 0), 2)
 			
-			#*****once object has been detected, lockon with tracker
+			#************once object has been detected, lockon with tracker
 			#---will need to decide targets before this point, 
-			
 			#initBB = boxes[0]
 			(x, y, w, h) = [int(v) for v in rects[0]]
 			initBB = (x,y,w,h) #only takes first thing detected
 			print('***********RECOGNIZE ',name,' *****************' )
-			#print("face detect gives ",initBB)
-			
+			#print("face detect gives ",initBB)		
 			if not initialized:	
 				initialized = tracker.init(frame,initBB)
 				#print(initialized)
+			#******************************
+	
 	# display the image to our screen
 	cv2.imshow("Frame", frame)
-	key = cv2.waitKey(1) & 0xFF
-	
-	if key==ord("r"): #reset initBB
-		initBB = None
-		#tracker = cv2.TrackerMOSSE_create()
-#might need to reset tracker as well
-	#	print("Restarting search")
+	key = cv2.waitKey(1) & 0xFF	
+
+	if key == ord("t"):
+		print('Starting timer for detection')
+		starttime=time.time()
+
 	# if the `q` key was pressed, break from the loop
 	if key == ord("q"):
 		break
-
 	# update the FPS counter
 	fps.update()
 
@@ -213,7 +198,6 @@ while True:
 fps.stop()
 print("[INFO] elasped time: {:.2f}".format(fps.elapsed()))
 print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
-
 # do a bit of cleanup
 cv2.destroyAllWindows()
 vs.stop()
